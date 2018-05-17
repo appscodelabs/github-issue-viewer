1dcbcc258751bbc386a93eb6e2a26cb1cdae8055
sajib <sajib@appscode.com>
2018-05-17 16:00:54 +0600

    Make generic MyVuetable passing props from App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,42 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <div class="ui container">
-      <my-vuetable></my-vuetable>
+      <my-vuetable
+        apiUrl="https://vuetable.ratiw.net/api/users"
+        :fields="fields"
+        :sort-order="sortOrder"
+        :append-params="moreParams"
+        detail-row-component="my-detail-row"
+      ></my-vuetable>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
 import MyVuetable from './components/MyVuetable';
+import FieldDefs from './components/FieldDefs';
+import DetailRow from './components/DetailRow';
+
+Vue.component('my-detail-row', DetailRow);
 
 export default {
   name: 'app',
   components: {
     MyVuetable,
+  },
+  data() {
+    return {
+      fields: FieldDefs,
+      sortOrder: [
+        {
+          field: 'email',
+          sortField: 'salary',
+          direction: 'asc',
+        },
+      ],
+      moreParams: {},
+    };
   },
 };
 </script>
--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -2,15 +2,16 @@
   <div>
     <filter-bar></filter-bar>
     <Vuetable ref="vuetable"
-    api-url="https://vuetable.ratiw.net/api/users"
+    :api-url="apiUrl"
     :fields="fields"
-    :sort-order="sortOrder"
     pagination-path=""
     :per-page="10"
+    :multi-sort="true"
+    :sort-order="sortOrder"
+    :append-params="appendParams"
     detail-row-component="my-detail-row"
     @vuetable:cell-clicked="onCellClicked"
     @vuetable:pagination-data="onPaginationData"
-    :append-params="moreParams"
     >
       <template slot="actions" scope="props">
         <div class="custom-actions">
@@ -48,24 +49,49 @@ import VuetablePagination from 'vuetable-2/src/components/VuetablePaginationDrop
 import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo';
 // import CustomActions from './CustomActions';
 import VueEvents from 'vue-events';
-import DetailRow from './DetailRow';
+// import DetailRow from './DetailRow';
 import FilterBar from './FilterBar';
-import FieldDefs from './FieldDefs';
+// import FieldDefs from './FieldDefs';
 
 Vue.use(VueEvents);
-Vue.component('my-detail-row', DetailRow);
+// Vue.component('my-detail-row', DetailRow);
 Vue.component('filter-bar', FilterBar);
 
 export default {
+  name: 'my-vuetable',
   components: {
     Vuetable,
     VuetablePagination,
     VuetablePaginationInfo,
   },
+  props: {
+    apiUrl: {
+      type: String,
+      required: true,
+    },
+    fields: {
+      type: Array,
+      required: true,
+    },
+    sortOrder: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    appendParams: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    detailRowComponent: {
+      type: String,
+    },
+  },
   data() {
     return {
-      fields: FieldDefs,
-      moreParams: {},
+      // fields: FieldDefs,
     };
   },
   mounted() {
@@ -93,13 +119,11 @@ export default {
     },
     onFilterSet(filterText) {
       console.log('filter-set: ', filterText);
-      this.moreParams = {
-        filter: filterText,
-      };
+      this.appendParams.filter = filterText;
       Vue.nextTick(() => this.$refs.vuetable.refresh());
     },
     onFilterReset() {
-      this.moreParams = {};
+      delete this.appendParams.filter;
       Vue.nextTick(() => this.$refs.vuetable.refresh());
       console.log('filter-reset: ');
     },
